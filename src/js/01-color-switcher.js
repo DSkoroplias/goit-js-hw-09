24c2fac341cbda0c7f9dcfca0a7181122b916614
Dmytro Skoroplias <87.dimuch@gmail.com>
2022-11-30 10:55:53 +0200

    done 01homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const DELAY = 1000;
+let colorTimeId = null;
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+refs.startBtn.addEventListener('click', clickStart);
+refs.stopBtn.addEventListener('click', clickStop);
+
+function clickStart() {
+  colorTimeId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, DELAY);
+  refs.startBtn.setAttribute('disabled', true);
+  refs.stopBtn.removeAttribute('disabled');
+
+  console.log('start');
+}
+
+function clickStop() {
+  clearInterval(colorTimeId);
+  document.body.style.background = 'none';
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+  console.log('stop');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
