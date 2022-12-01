bff69ba0695735c1bd2c09aeda54eff7348033f4
Dmytro Skoroplias <87.dimuch@gmail.com>
2022-12-01 13:02:39 +0200

    done 03homework

---
+++ src/js/02-timer.js
@@ -0,0 +1,112 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  daysEl: document.querySelector('span[data-days]'),
+  hoursEl: document.querySelector('span[data-hours]'),
+  minutesEl: document.querySelector('span[data-minutes]'),
+  secondsEl: document.querySelector('span[data-seconds]'),
+};
+
+refs.startBtn.setAttribute('disabled', true);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] <= Date.now()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      refs.startBtn.setAttribute('disabled', true);
+    } else {
+      userSelectedDates = selectedDates[0];
+      refs.startBtn.removeAttribute('disabled');
+    }
+    console.log(userSelectedDates);
+  },
+};
+
+const resultFlatpickr = flatpickr('input#datetime-picker', options);
+
+class Timer {
+  constructor({ onTick }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.onTick = onTick;
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      resultFlatpickr.selectedDates[0];
+
+      const selectedDate = resultFlatpickr.selectedDates[0];
+
+      const currentDate = Date.now();
+
+      const deltaTime = selectedDate - currentDate;
+
+      const time = this.convertMs(deltaTime);
+
+      this.onTick(time);
+
+      if (deltaTime < 1000) {
+        this.stop();
+        return;
+      }
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+  }
+
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.addLeadingZero(
+      Math.floor(((ms % day) % hour) / minute)
+    );
+    // Remaining seconds
+    const seconds = this.addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+
+    return { days, hours, minutes, seconds };
+  }
+
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+  }
+}
+
+const timer = new Timer({
+  onTick: updateClockFace,
+});
+
+function updateClockFace({ days, hours, minutes, seconds }) {
+  refs.daysEl.textContent = `${days}`;
+  refs.hoursEl.textContent = `${hours}`;
+  refs.minutesEl.textContent = `${minutes}`;
+  refs.secondsEl.textContent = `${seconds}`;
+}
+
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+  delayInputEl: document.querySelector('input[name="delay"]'),
+  stepInputEl: document.querySelector('input[name="step"]'),
+  amountInputEl: document.querySelector('input[name="amount"]'),
+};
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  let delayValue = Number(refs.delayInputEl.value);
+  let stepValue = Number(refs.stepInputEl.value);
+  let amountValue = Number(refs.amountInputEl.value);
+
+  for (let i = 0; i < amountValue; i += 1) {
+    createPromise(i + 1, i * delayValue + stepValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
+  evt.target.reset();
 }
